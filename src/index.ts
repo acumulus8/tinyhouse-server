d239c4bb5b95eeca9f2612900a8e606980c37120
acmulus8 <timwilburn8@gmail.com>
2021-12-30 20:10:10 -0800

    updated Rest API to Apollo Server API, defined first basic schema, installed plugin landing page to workaround the sandbox issue, updated node and resolved issue with dependency versions

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,22 @@
 import express from "express";
-import bodyParser from "body-parser";
-import { listings } from "./listings";
+import { ApolloServer } from "apollo-server-express";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import { schema } from "./graphql";
 
 const app = express();
 const port = 9000;
+const server = new ApolloServer({ 
+  schema,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground] 
+});
 
-app.use(bodyParser.json())
+server.start()
+  .then(() => {
+    server.applyMiddleware({ app, path: '/api' })
+    app.listen(port);
+    console.log(`[app]: http://localhost:${port}`);
+  })
 
-app.get("/listings", (_req, res) => {
-  return res.send(listings);
-})
 
-app.post("/delete-listing", (req, res) => {
-  const id: string = req.body.id;
 
-  for (let i = 0; i < listings.length; i++) {
-    if (listings[i].id === id) {
-      return res.send(listings.splice(i, 1))
-    }
-  }
 
-  return res.send("failed to delete listing");
-})
-
-app.listen(port);
-
-console.log(`[app]: http://localhost:${port}`);
