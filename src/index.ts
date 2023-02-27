b52b071a9a1a084269d20b75b9772b19263368ca
Tim <timwilburn8@gmail.com>
2023-02-27 13:00:11 -0500

    fix: updated start script for deployment

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import { typeDefs, resolvers } from "./graphql";
 import { connectDatabase } from "./database";
-import { myMocky } from "./lib/utils";
+// import { myMocky } from "./lib/utils";
 
 const port = process.env.PORT;
 
@@ -19,7 +19,7 @@ const mount = async (app: Application) => {
 	app.use(compression());
 	app.use(express.static(`${__dirname}/client`));
 	app.get("/*", (_req, res) => res.sendFile(`${__dirname}/client/index.html`));
-	app.post("/myMocky", (_req, res) => res.send(myMocky));
+	// app.post("/myMocky", (_req, res) => res.send(myMocky));
 
 	const server = new ApolloServer({
 		typeDefs,
